8e6a7fbad8af49c6260ec5a7bff608c8cb414464
isamar43 <crissangulo932@gmail.com>
2025-02-03 21:15:46 +0000

    se agrego uniajc al mensaje

--- clase1/ejecutar.java
+++ clase1/ejecutar.java
@@ -1,6 +1,6 @@
 public class ejecutar {
 
     public static void main(String[] args) {
-        System.out.println("¡¡hello mundo!!");
+        System.out.println("¡¡hello mundo!! UNIAJC");
     }
 }
